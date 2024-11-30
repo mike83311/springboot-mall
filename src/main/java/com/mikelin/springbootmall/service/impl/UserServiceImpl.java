1e5aac00e1daf7ad9106a70cc48a39f33af5c775
mike83311 <mike83311@gmail.com>
2024-11-30 16:55:09 +0800

    user function - hash by md5

--- src/main/java/com/mikelin/springbootmall/service/impl/UserServiceImpl.java
+++ src/main/java/com/mikelin/springbootmall/service/impl/UserServiceImpl.java
@@ -10,7 +10,10 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
+import org.springframework.util.DigestUtils;
 import org.springframework.web.server.ResponseStatusException;
+
+import java.nio.charset.StandardCharsets;
 
 
 @Component
@@ -31,6 +34,9 @@ public class UserServiceImpl implements UserService {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
         }
 
+        String hashedPassword = DigestUtils.md5DigestAsHex(userRegisterRequest.getPassword().getBytes());
+        userRegisterRequest.setPassword(hashedPassword);
+
         return userDao.createUser(userRegisterRequest);
     }
 
@@ -48,7 +54,8 @@ public class UserServiceImpl implements UserService {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
         }
 
-        if (!user.getPassword().equals(userLoginRequest.getPassword())) {
+        String hashedPassword = DigestUtils.md5DigestAsHex(userLoginRequest.getPassword().getBytes());
+        if (!user.getPassword().equals(hashedPassword)) {
             log.warn("the password of {} does not match", userLoginRequest.getEmail());
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
         }
